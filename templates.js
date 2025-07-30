2ba64bbe4309754b1f956ab837c87a8c57e78430
Dustin Condello <dustincondello@googlemail.com>
2025-07-30 18:34:19 +0200

    toUpperCase for types and css style improved

--- templates.js
+++ templates.js
@@ -5,9 +5,9 @@ function  renderPokemon(pokemonData) {
     let content = `
                     <div class="content-pokemon">
                     <h4>${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h4>
-                    <p>#${pokemonData.id}</p>
+                    <p>N° ${pokemonData.id}</p>
                     <ul>
-                        ${pokemonData.types.map(type => `<li>${type.type.name}</li>`).join('')}
+                     ${pokemonData.types.map(type => `<li>${type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</li>`).join('')}
                     </ul>
                     <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
                     </div>
